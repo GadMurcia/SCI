68ab236779eba976637f0ab49e5f2301a6a9a59c
GadMurcia <guillermox001@gmail.com>
2021-09-29 23:53:40 -0600

    impresion de reportes de compras y existencias

--- inventarios-web/src/main/java/net/delsas/inventarios/Controllers/reportes1Ctr.java
+++ inventarios-web/src/main/java/net/delsas/inventarios/Controllers/reportes1Ctr.java
@@ -4,9 +4,22 @@
  */
 package net.delsas.inventarios.Controllers;
 
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfPage;
+import com.lowagie.text.pdf.PdfWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -28,6 +41,7 @@ import net.delsas.inventarios.beans.DetalleVentasFacadeLocal;
 import net.delsas.inventarios.beans.GiroDeCajaFacadeLocal;
 import net.delsas.inventarios.beans.InventarioFacadeLocal;
 import net.delsas.inventarios.beans.MiscFacadeLocal;
+import net.delsas.inventarios.beans.VentasFacadeLocal;
 import net.delsas.inventarios.entities.Compras;
 import net.delsas.inventarios.entities.DetalleCompra;
 import net.delsas.inventarios.entities.DetalleVentas;
@@ -39,6 +53,9 @@ import net.delsas.inventarios.optional.Existencias;
 import net.delsas.inventarios.optional.auxiliarCtr;
 import net.delsas.inventarios.optional.reporteFacturasCompras;
 import org.primefaces.PrimeFaces;
+import org.primefaces.component.datatable.DataTable;
+import org.primefaces.component.export.ExportConfiguration;
+import org.primefaces.component.export.Exporter;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.event.TabChangeEvent;
 
@@ -76,9 +93,9 @@ public class reportes1Ctr extends auxiliarCtr implements Serializable {
     @EJB
     private GiroDeCajaFacadeLocal gdcfl;
     @EJB
-    private DetalleCompraFacadeLocal dcfl1;
-    @EJB
     private ComprasFacadeLocal cfl;
+    @EJB
+    private VentasFacadeLocal vfl;
 
     @PostConstruct
     public void init() {
@@ -330,7 +347,7 @@ public class reportes1Ctr extends auxiliarCtr implements Serializable {
             fin.setHours(23);
             fin.setMinutes(59);
             fin.setSeconds(59);
-            List<DetalleCompra> dc = dcfl1.findByPeriodoFechas(inicio, fin);
+            List<DetalleCompra> dc = dcfl.findByPeriodoFechas(inicio, fin);
             compra = new Compras();
             compra.setValor(dc.stream()
                     .map(d -> d.getCostoUnitario().multiply(new BigDecimal(d.getCantidad())))
@@ -385,4 +402,239 @@ public class reportes1Ctr extends auxiliarCtr implements Serializable {
                 : (getDoc(facSelected.getFactura(), facSelected.getExt()));
     }
 
+    public void generarRepVentas1() {
+        if (inicio != null) {
+            periodo = !(fin == null || fin.equals(inicio));
+            fin = fin == null || fin.before(inicio) ? new Date(inicio.getTime()) : fin;
+            fin.setHours(23);
+            fin.setMinutes(59);
+            fin.setSeconds(59);
+            girosUndía = gdcfl.findByPeriodoYSucursal(inicio, fin, 1);
+            giro = new GiroDeCaja();
+            girosUndía.forEach(g -> {
+                List<Ventas> vs = vfl.findByGiroCaja(g.getIdGiroDeCaja());
+                Ventas v = new Ventas();
+                v.setValor(vs.stream().map(Ventas::getValor).reduce(BigDecimal::add).orElseGet(() -> BigDecimal.ZERO));
+                List<DetalleVentas> dvs = new ArrayList<>();
+                vs.forEach(v0 -> {
+                    List<DetalleVentas> dvs1 = dvfl.findByVenta(v0.getVentasPK());
+                    dvs1.forEach(dvs10 -> {
+                        llenarDetallVentas(dvs, dvs10);
+                    });
+                });
+                ordenarPrecioPromedio(dvs);
+                v.setDetalleVentasList(dvs);
+                int idx = girosUndía.indexOf(g);
+                g.setVentasList(new ArrayList<>());
+                g.getVentasList().add(v);
+                girosUndía.set(idx, g);
+            });
+
+            if (periodo) {
+                giro.setRetiros(girosUndía.stream().mapToDouble(GiroDeCaja::getRetiros).sum());
+                giro.setFaltantes(girosUndía.stream().mapToDouble(GiroDeCaja::getFaltantes).sum());
+                giro.setExcedentes(girosUndía.stream().mapToDouble(GiroDeCaja::getExcedentes).sum());
+                giro.setDetalleRetiros(girosUndía.stream().map(GiroDeCaja::getDetalleRetiros).collect(Collectors.joining("\n")));
+                giro.setVentasList(new ArrayList<>());
+                giro.getVentasList().add(new Ventas());
+                giro.getVentasList().get(0).setValor(BigDecimal.ZERO);
+                girosUndía.stream().forEach(g -> g.getVentasList().stream()
+                        .forEach(v -> giro.getVentasList().get(0)
+                        .setValor(giro.getVentasList().get(0).getValor()
+                                .add(v.getValor()))));
+                List<DetalleVentas> d = new ArrayList<>();
+                girosUndía.stream().forEach(g -> g.getVentasList().stream().forEach(v -> v.getDetalleVentasList().stream().forEach(gvd -> {
+                    llenarDetallVentas(d, gvd);
+                })));
+                ordenarPrecioPromedio(d);
+                giro.getVentasList().get(0).setDetalleVentasList(d);
+            }
+        } else {
+            giro = null;
+        }
+    }
+
+    private void ordenarPrecioPromedio(List<DetalleVentas> dvs) {
+        dvs.forEach(dvs0 -> {
+            int idx = dvs.indexOf(dvs0);
+            DetalleVentas get = dvs.get(idx);
+            get.setPrecioUnitario(new BigDecimal(redondeo2decimales(get.getPrecioUnitario().doubleValue() / get.getDetalleVentasPK().getProducto())));
+            get.getDetalleVentasPK().setProducto(get.getInventario().getIdInventario());
+            dvs.set(idx, get);
+        });
+    }
+
+    private void llenarDetallVentas(List<DetalleVentas> dvs, DetalleVentas dvs10) {
+        List<DetalleVentas> previo = dvs.stream().filter(dvs0 -> dvs0.getInventario().getIdInventario().equals(dvs10.getInventario().getIdInventario())).collect(Collectors.toList());
+        if (previo.isEmpty()) {
+            dvs10.getDetalleVentasPK().setProducto(1);
+            dvs.add(dvs10);
+        } else {
+            DetalleVentas get = dvs.get(0);
+            int idx = dvs.indexOf(get);
+            get.setCantidad(get.getCantidad() + dvs10.getCantidad());
+            get.setPrecioUnitario(get.getPrecioUnitario().add(dvs10.getPrecioUnitario()));
+            get.getDetalleVentasPK().setProducto(get.getDetalleVentasPK().getProducto() + 1);
+            dvs.set(idx, get);
+        }
+    }
+
+    public Exporter<DataTable> getPdfExporterExistencias() {
+        getMatrices();
+        getSucursales();
+        return new Exporter<DataTable>() {
+            @Override
+            public void export(FacesContext fc, List<DataTable> list, OutputStream out, ExportConfiguration ec) {
+                Document pdf;
+                PdfWriter writer;
+                try {
+                    PdfPTable t = new PdfPTable(8);
+
+                    t.addCell(getTextCell(sucSel.getNombre(), 8, 1, false, false, 18, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 8, 1, false, false, 18, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 8, 1, false, false, 18, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+
+                    t.addCell(getTextCell("Reporte de existencias de productos", 8, 1, false, false, 16, Font.BOLDITALIC, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 8, 1, false, false, 18, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+
+                    t.addCell(getTextCell("Producto", 2, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Cantidad", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Costo unitario", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Costo de producto", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Precio unitario", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Valor de venta", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Utilidad", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+
+                    List<Existencias> ex = getExistencias();
+                    ex.stream().forEachOrdered(e -> {
+                        t.addCell(getTextCell(e.getNombre(), 2, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell(e.getExistencias() + "", 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell("" + e.getCostoAVG(), 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell("" + e.getCostoTotal(), 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell("" + e.getPrecioAVG(), 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell("" + e.getValorTotal(), 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell("" + e.getUtilidad(), 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    });
+                    t.addCell(getTextCell("", 4, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Costo total del inventario: $", 3, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_RIGHT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("" + ex.stream().mapToDouble(Existencias::getCostoTotal).sum(), 1, 1, false, true, 12, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 4, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Valor de venta total del inventario: $", 3, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_RIGHT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("" + ex.stream().mapToDouble(Existencias::getValorTotal).sum(), 1, 1, false, true, 12, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 4, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Utilidad esperada total: $", 3, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_RIGHT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("" + ex.stream().mapToDouble(Existencias::getUtilidad).sum(), 1, 1, false, true, 12, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+
+                    t.addCell(getTextCell("", 8, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 8, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 6, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Generado el " + (new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a").format(new Date())) + " /S.C.I./", 2, 1, false, false, 10, Font.NORMAL, PdfPCell.ALIGN_RIGHT, PdfPCell.ALIGN_MIDDLE));
+                    //OutputStream out = getOutputStream(fc, ec, "application/pdf");
+                    pdf = new Document(PageSize.LETTER.rotate());
+                    writer = PdfWriter.getInstance(pdf, out);
+                    pdf.open();
+                    t.setWidthPercentage(100);
+                    pdf.add(t);
+                    pdf.setMargins(1, 1, 1, 1);
+                    pdf.close();
+                    writer.flush();
+                } catch (DocumentException e) {
+                    Logger.getLogger(reportes1Ctr.class.getName()).log(Level.SEVERE, null, e);
+                    fc.addMessage("form0:msgs",
+                            new FacesMessage(FacesMessage.SEVERITY_FATAL, "Eror!!!", "No se pudo generar el reporte."));
+                }
+
+            }
+
+            @Override
+            public String getContentType() {
+                return "application/pdf";
+            }
+
+            @Override
+            public String getFileExtension() {
+                return "pdf";
+            }
+        };
+    }
+
+    public Exporter<DataTable> getPdfExporterCompras() {
+        getMatrices();
+        getSucursales();
+        return new Exporter<DataTable>() {
+            @Override
+            public void export(FacesContext fc, List<DataTable> list, OutputStream out, ExportConfiguration ec) {
+                Document pdf;
+                PdfWriter writer;
+                try {
+                    PdfPTable t = new PdfPTable(5);
+
+                    t.addCell(getTextCell(sucSel.getNombre(), 5, 1, false, false, 18, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 5, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Reporte de Compras", 5, 1, false, false, 16, Font.BOLDITALIC, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 5, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Periodo del reporte: ", 2, 1, false, false, 14, Font.BOLD, PdfPCell.ALIGN_RIGHT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell(new SimpleDateFormat("dd/MM/YYYY").format(inicio) + (fin.equals(inicio) ? "" : " al " + new SimpleDateFormat("dd/MM/YYYY").format(fin)), 3, 1, false, false, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("", 5, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+
+                    t.addCell(getTextCell("Nombre del roducto", 2, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Cantidad", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Costo unitario", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Costo del producto", 1, 1, false, true, 14, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+
+                    compra.getDetalleCompraList().stream().forEachOrdered(d -> {
+                        t.addCell(getTextCell(d.getInventario().getProducto(), 2, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell(d.getCantidad() + "", 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell("" + d.getCostoUnitario().doubleValue(), 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                        t.addCell(getTextCell("" + redondeo2decimales((d.getCantidad() * d.getCostoUnitario().doubleValue())), 1, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+                    });
+                    t.addCell(getTextCell("", 2, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Total de compras: $", 2, 1, false, true, 12, Font.NORMAL, PdfPCell.ALIGN_RIGHT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("" + compra.getDetalleCompraList().stream().mapToDouble(d -> d.getCantidad() * d.getCostoUnitario().doubleValue()).sum(), 1, 1, false, true, 12, Font.BOLD, PdfPCell.ALIGN_CENTER, PdfPCell.ALIGN_MIDDLE));
+
+                    t.addCell(getTextCell("", 3, 1, false, false, 12, Font.NORMAL, PdfPCell.ALIGN_LEFT, PdfPCell.ALIGN_MIDDLE));
+                    t.addCell(getTextCell("Generado el " + (new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a").format(new Date())) + " /S.C.I./", 2, 1, false, false, 10, Font.NORMAL, PdfPCell.ALIGN_RIGHT, PdfPCell.ALIGN_MIDDLE));
+                    
+                    
+                    pdf = new Document(PageSize.LETTER);
+                    writer = PdfWriter.getInstance(pdf, out);
+                    pdf.open();
+                    t.setWidthPercentage(100);
+                    pdf.add(t);
+                    pdf.setMargins(1, 1, 1, 1);
+                    pdf.add(Phrase.getInstance("\n\n\n\nListado de facturas de compras\n\n\n\n"));
+                    getFacturas().stream().forEachOrdered(f->{
+                        try {                            
+                            Image i = Image.getInstance(f.getFactura());
+                            i.scalePercent(50);
+                            i.setAlignment(Image.ALIGN_CENTER);
+                            pdf.add(i);
+                            pdf.add(Phrase.getInstance("\n\n\n\n"));
+                        } catch (BadElementException | IOException ex) {
+                            Logger.getLogger(reportes1Ctr.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (DocumentException ex) {
+                            Logger.getLogger(reportes1Ctr.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    });
+                    pdf.close();
+                    writer.flush();
+                } catch (DocumentException e) {
+                    Logger.getLogger(reportes1Ctr.class.getName()).log(Level.SEVERE, null, e);
+                    fc.addMessage("form0:msgs",
+                            new FacesMessage(FacesMessage.SEVERITY_FATAL, "Eror!!!", "No se pudo generar el reporte."));
+                }
+
+            }
+
+            @Override
+            public String getContentType() {
+                return "application/pdf";
+            }
+
+            @Override
+            public String getFileExtension() {
+                return "pdf";
+            }
+        };
+    }
 }
--- inventarios-web/src/main/java/net/delsas/inventarios/optional/auxiliarCtr.java
+++ inventarios-web/src/main/java/net/delsas/inventarios/optional/auxiliarCtr.java
@@ -5,11 +5,22 @@
  */
 package net.delsas.inventarios.optional;
 
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Image;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Named;
 import net.delsas.inventarios.beans.DetalleCompraFacadeLocal;
@@ -17,7 +28,11 @@ import net.delsas.inventarios.beans.DetalleVentasFacadeLocal;
 import net.delsas.inventarios.beans.InventarioFacadeLocal;
 import net.delsas.inventarios.entities.Inventario;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.poi.util.IOUtils;
+import org.primefaces.component.export.ExportConfiguration;
 import org.primefaces.event.CellEditEvent;
+import org.primefaces.util.ComponentUtils;
+import org.primefaces.util.Constants;
 
 /**
  *
@@ -67,5 +82,50 @@ public class auxiliarCtr implements Serializable {
         String a = doc == null ? ""
                 : "data:" + ex + ";base64, " + new String(Base64.encodeBase64(doc));
         return a;
+    }
+    
+     public OutputStream getOutputStream(FacesContext fc, ExportConfiguration ec, String tipoArchivo) throws IOException {
+        fc.getExternalContext().setResponseContentType(tipoArchivo);
+        fc.getExternalContext().setResponseHeader("Expires", "0");
+        fc.getExternalContext().setResponseHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
+        fc.getExternalContext().setResponseHeader("Pragma", "public");
+        fc.getExternalContext().setResponseHeader("Content-disposition", ComponentUtils.createContentDisposition("attachment", ec.getOutputFileName() + ".pdf"));
+        fc.getExternalContext().addResponseCookie(Constants.DOWNLOAD_COOKIE, "true", Collections.<String, Object>emptyMap());
+        return fc.getExternalContext().getResponseOutputStream();
+    }
+
+    public PdfPCell getCellWithImagen(FacesContext fc, String nombreImg, int colSpan, int rowSpan, boolean borde, int imgScale, int aligment) {
+        PdfPCell c1;
+        String logo = fc.getExternalContext().getRealPath("") + File.separator + "resources" + File.separator + "img" + File.separator + nombreImg;
+        try {
+            InputStream is = new FileInputStream(logo);
+            Image i = Image.getInstance(IOUtils.toByteArray(is));
+            i.scalePercent(imgScale);
+            c1 = new PdfPCell(i);
+        } catch (BadElementException | IOException ex) {
+            c1 = new PdfPCell(new Phrase(""));
+        }
+        c1.setColspan(colSpan);
+        c1.setRowspan(rowSpan);
+        if (!borde) {
+            c1.setBorder(0);
+        }
+        c1.setHorizontalAlignment(aligment);
+        return c1;
+    }
+
+    public PdfPCell getTextCell(String txt, int cSpan, int rSpan, boolean nWrap, boolean borde, int tSize, int tStyle, int hAligmnet, int vAligment) {
+        PdfPCell c1 = new PdfPCell(new Phrase(txt));
+        c1.setRowspan(rSpan);
+        c1.setColspan(cSpan);
+        c1.setNoWrap(nWrap);
+        if (!borde) {
+            c1.setBorder(0);
+        }
+        c1.getPhrase().getFont().setSize(tSize);
+        c1.getPhrase().getFont().setStyle(tStyle);
+        c1.setHorizontalAlignment(hAligmnet);
+        c1.setVerticalAlignment(vAligment);
+        return c1;
     }
 }
